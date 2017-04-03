372e750b7c695dc159a3540b56abba3badeee161
Hitendra Mac <hitendra.spaceo@gmail.com>
2017-04-03 18:17:01 +0530

    added load texture/image method

---
+++ OpenGLRay/ImageHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ImageHelper.swift
+//  OpenGLRay
+//
+//  Created by Hitendra Mac on 03/04/17.
+//  Copyright © 2017 Hitendra Mac. All rights reserved.
+//
+
+import UIKit
+
+class ImageHelper: NSObject {
+
+}
